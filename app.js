a771a42152a5b62568f89a89570135f8d057214e
Ekaterina Suslova <>
2020-04-02 21:01:59 +0200

    test examples

--- app.js
+++ app.js
@@ -28,3 +28,5 @@ app.use('/users', usersRoutes);
 app.listen(config.port, () => {
     console.log('Server running on port %s', config.port);
 });
+
+module.exports = app;
--- controllers/users.js
+++ controllers/users.js
@@ -6,38 +6,38 @@ exports.login = ((req, res) => {
         message: "login",
         name: req.query.name,
         password: req.query.password,
-    })
+    });
 });
 
 exports.logout = ((req, res) => {
     res.json({
         message: "logout",
-    })
+    });
 });
 
 exports.getUser = ((req, res) => {
     res.json({
         message: "getUser",
         name: req.params.username,
-    })
+    });
 });
 
 exports.putUser = ((req, res) => {
     res.json({
         message: "update User",
         name: req.params.username,
-    })
+    });
 });
 
 exports.deleteUser = ((req, res) => {
     res.json({
         message: "delete User",
         name: req.params.username,
-    })
+    });
 });
 
 exports.postUser = ((req, res) => {
     res.json({
         message: "Add new User",
-    })
+    });
 });
