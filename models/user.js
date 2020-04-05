b1e1a6369f9dc2a20ec14610e8d960161c4cfaf9
Ekaterina Suslova <>
2020-04-05 14:53:07 +0200

    heroku tests (disable seed)

--- models/user.js
+++ models/user.js
@@ -108,19 +108,19 @@ module.exports = (dbc, DataTypes) => {
 //     User.sync().then(function () {
 //         // Table created
 //         return
-        User.create({
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'johndoe@gmail.com',
-            username: 'johndoe',
-            password: '1234567',
-        })
-            .then(function () {
-                console.log("user created");
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+//         User.create({
+//             firstName: 'John',
+//             lastName: 'Doe',
+//             email: 'johndoe@gmail.com',
+//             username: 'johndoe',
+//             password: '1234567',
+//         })
+//             .then(function () {
+//                 console.log("user created");
+//             })
+//             .catch(function (err) {
+//                 console.log(err);
+//             });
     // });
 
     return User;
