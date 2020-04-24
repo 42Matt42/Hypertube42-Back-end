7926c702f11c936a08382fd45792b81eedda5524
Fbartoli <fbartoli@monaco.edu>
2020-04-24 16:57:42 +0200

    oauth facebook + user creation

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -58,13 +58,27 @@ exports.redirectGitHub = async (req, res) => {
                 code: code,
             }
         )
+        let token = await response.data.split('&')[0].split('=')[1]
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+        let userData = await axios.get('https://api.github.com/user')
+        const result = await db.user.findOrCreate({
+            where: {
+                firstName: userData.data.name.split(' ')[0],
+                lastName: userData.data.name.split(' ')[1],
+                email: userData.data.email,
+                username: userData.data.login,
+                photo: userData.data.avatar_url,
+                password: 'àchangerplustard',
+            },
+        })
         let exp = Math.floor(Date.now() / 1000) + 60 * 60 * 24
         const jwt_token = jwt.sign(
             {
+                exp,
                 data: {
-                    id: 999,
-                    username: 'usertest',
-                    token: response.data,
+                    username: result[0].dataValues.username,
+                    token: result[0].dataValues.access_token,
+                    photo: result[0].dataValues.photo,
                 },
             },
             config.jwt
@@ -84,19 +98,33 @@ exports.redirectFacebook = async (req, res) => {
             `https://graph.facebook.com/v6.0/oauth/access_token?client_id=1245062255689643&client_secret=aa162bbac8aa37524abe9216c5cdeb85&code=${code}&redirect_uri=http://localhost:5555/oauth/fb`
         )
         let exp = Math.floor(Date.now() / 1000) + 60 * 60 * 24
+        let userData = await axios.get(
+            `https://graph.facebook.com/v6.0/me?fields=last_name,picture{url},email,name&access_token=${response.data.access_token}`
+        )
+        const result = await db.user.findOrCreate({
+            where: {
+                firstName: userData.data.name.split(' ')[0],
+                lastName: userData.data.name.split(' ')[1],
+                email: userData.data.email,
+                username: userData.data.name,
+                photo: userData.data.picture.data.url,
+                password: 'àchangerplustard',
+            },
+        })
         const jwt_token = jwt.sign(
             {
-                exp: exp,
+                exp,
                 data: {
-                    id: 999,
-                    username: 'facebook',
-                    token: response.data,
+                    username: result[0].dataValues.username,
+                    token: result[0].dataValues.access_token,
+                    photo: result[0].dataValues.photo,
                 },
             },
             config.jwt
         )
         return res.redirect('http://localhost:8080?code=' + jwt_token)
     } catch (error) {
+        console.log(error)
         return res.status(500).json({
             error: error,
         })
