f4ccf8a4d7deccf3343a3a0690cc40432d7837d4
Fbartoli <fbartoli@monaco.edu>
2020-04-30 16:56:39 +0200

    test alias heroku

--- models/tempEmail.js
+++ models/tempEmail.js
@@ -1,45 +1,43 @@
 // const { Sequelize, DataTypes } = require('sequelize');
 const User = require('./user')
 
-
 module.exports = (dbc, DataTypes) => {
     const TempEmail = dbc.define('tempEmail', {
-            email: {
-                type: DataTypes.STRING,
-                // unique: true,
-                allowNull: true,
-                validate: {
-                    isEmail: {
-                        args: true,
-                        msg: "Please provide a valid email"
-                    },
-                    isNull: function (val) {
-                        if (!val) {
-                            throw new Error("Please provide email")
-                        }
+        email: {
+            type: DataTypes.STRING,
+            // unique: true,
+            allowNull: true,
+            validate: {
+                isEmail: {
+                    args: true,
+                    msg: 'Please provide a valid email',
+                },
+                isNull: function (val) {
+                    if (!val) {
+                        throw new Error('Please provide email')
                     }
-                }
-            },
-            token: {
-                type: DataTypes.UUID,
-                defaultValue: DataTypes.UUIDV4,
-            },
-            token_creation: {
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
+                },
             },
         },
-    );
+        token: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        token_creation: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+    })
 
     TempEmail.associate = function (models) {
         models.tempEmail.belongsTo(models.user, {
-            onDelete: "CASCADE",
+            onDelete: 'CASCADE',
             foreignKey: {
                 allowNull: false,
-                unique: true
-            }
-        });
-    };
+                unique: true,
+            },
+        })
+    }
 
-    return TempEmail;
+    return TempEmail
 }
